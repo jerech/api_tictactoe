a20d1d018313ec0807a6a02877b7f1d588a9ed79
Jeremias Chaparro <jeremiaschaparro@gmail.com>
2019-10-05 16:35:08 -0300

    Configuraciones Globales y Swagger

---
+++ src/main/java/com/jerech/tictactoe/config/GlobalConfig.java
@@ -0,0 +1,18 @@
+/**
+ * @author jeremiaschaparro@gmail.com 
+ *
+ */
+package com.jerech.tictactoe.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GlobalConfig {
+	@Bean
+	public ModelMapper modelMapper() {
+	    return new ModelMapper();
+	}
+}
+
