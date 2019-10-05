9c28ae9c232a0842167648a68aa583b0df267b00
Jeremias Chaparro <jeremiaschaparro@gmail.com>
2019-10-05 16:14:55 -0300

    Creando api spring boot

---
+++ src/main/java/com/jerech/tictactoe/TicTacToeApplication.java
@@ -0,0 +1,16 @@
+package com.jerech.tictactoe;
+/*
+ * @author JereCh
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeApplication.class, args);
+	}
+	
+}
